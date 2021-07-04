ef4f207ddd1419c4110b17abfc951f38e120d3b1
Błażej Bałdyga <125223@student.uni.opole.pl>
2021-07-04 16:37:25 +0200

    Test branch to delete vs github forced me to download from main and wont me drop any changes

---
+++ JBankCS/UserMenuWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JBankCS
+{
+    /// <summary>
+    /// Logika interakcji dla klasy UserMenuWindow.xaml
+    /// </summary>
+    public partial class UserMenuWindow : Window
+    {
+        public UserMenuWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
